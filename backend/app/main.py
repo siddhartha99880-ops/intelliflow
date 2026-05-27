3ea8b37a6003c4b5541cb0faaf309f8210e72742
Siddhartha Singh <siddhartha99880@gmail.com>
2026-05-27 21:45:46 +0530

    fix: resolve CORS issues by allowing all origin domains via regex

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,7 +48,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.backend_cors_origins,
+        allow_origin_regex=".*",
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
